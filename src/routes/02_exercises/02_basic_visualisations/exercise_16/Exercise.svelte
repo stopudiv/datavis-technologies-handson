86db1be4b060c4f4aa1b71635c1e54050d5e9d8d
stopudiv <ogrytsenko@gmail.com>
2023-08-21 18:49:21 +0100

    exercises_solutions

--- src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
@@ -6,7 +6,7 @@
     const innerHeight = height - margin.top - margin.bottom;
   </script>
   
-  <svg {width} {height}>
+  <svg viewbox="0 0 {width} {height}">
     <g transform="translate({margin.left}, {margin.top})">
       <circle cx={innerWidth / 2} cy={innerHeight / 2} r="20" fill="teal" />
     </g>
